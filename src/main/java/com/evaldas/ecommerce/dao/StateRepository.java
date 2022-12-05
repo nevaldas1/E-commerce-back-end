6b059eeae529499154448058456fefc0885662ef
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-05 17:06:14 +0200

    Create StateRepository class

---
+++ src/main/java/com/evaldas/ecommerce/dao/StateRepository.java
@@ -0,0 +1,15 @@
+package com.evaldas.ecommerce.dao;
+
+import com.evaldas.ecommerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+public interface StateRepository extends JpaRepository <State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+}
