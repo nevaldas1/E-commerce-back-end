c6453b1ec6f5b580e46f85a9dde2872de2e099fb
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-05 16:30:32 +0200

    Create State class

--- src/main/java/com/evaldas/ecommerce/entity/Country.java
+++ src/main/java/com/evaldas/ecommerce/entity/Country.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="country")
@@ -21,4 +22,7 @@ public class Country {
 
     @Column(name="name")
     private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
 }
