6e2642ede849aa35f4fc01bf9eb487ca961a37e5
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-06 20:07:14 +0200

    Create CheckoutController

---
+++ src/main/java/com/evaldas/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.evaldas.ecommerce.controller;
+
+import com.evaldas.ecommerce.dto.Purchase;
+import com.evaldas.ecommerce.dto.PurchaseResponse;
+import com.evaldas.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+
+    private CheckoutService checkoutService;
+
+    @Autowired   // Optional as single constructor
+    public CheckoutController (CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
