bd526435896fd98cdce6bde30a2facbeaabaac4b
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-06 17:00:31 +0200

    Create Order class

---
+++ src/main/java/com/evaldas/ecommerce/entity/Order.java
@@ -0,0 +1,44 @@
+package com.evaldas.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Entity
+@Table(name="orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name="total_quantity")
+    private int totalQuantity;
+
+    @Column(name="total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="date_created")
+    @CreationTimestamp                  //Automatically adds timestamp
+    private Date dateCreated;
+
+    @Column(name="date_updated")
+    @UpdateTimestamp
+    private Date dateUpdates;
+
+}
