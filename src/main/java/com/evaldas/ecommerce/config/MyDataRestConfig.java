8f9f30448c22db30688379f6ce9e292759ecee5b
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-08 01:31:35 +0200

    Restrict PATCH method

--- src/main/java/com/evaldas/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/evaldas/ecommerce/config/MyDataRestConfig.java
@@ -34,7 +34,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
