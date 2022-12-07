1cb18e23891beea8159322d44c6deffb3d3bb920
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-08 00:34:56 +0200

    Add unique customer check

--- src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
@@ -38,6 +38,14 @@ public class CheckoutServiceImpl implements CheckoutService{
         order.setBillingAddress(purchase.getBillingAddress());
 
         Customer customer = purchase.getCustomer();
+
+        String theEmail = customer.getEmail();
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+
+        if (customerFromDB != null) {
+            customer = customerFromDB;
+        }
+
         customer.add(order);
 
         customerRepository.save(customer);
