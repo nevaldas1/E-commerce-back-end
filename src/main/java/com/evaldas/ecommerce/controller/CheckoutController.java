5c9b79bcc5518063676fb7316b7b1c5ec90aea88
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-15 19:06:21 +0200

    Create new psot mapping for PaymentIntent

--- src/main/java/com/evaldas/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/evaldas/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.evaldas.ecommerce.controller;
 
+import com.evaldas.ecommerce.dto.PaymentInfo;
 import com.evaldas.ecommerce.dto.Purchase;
 import com.evaldas.ecommerce.dto.PurchaseResponse;
 import com.evaldas.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +28,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
