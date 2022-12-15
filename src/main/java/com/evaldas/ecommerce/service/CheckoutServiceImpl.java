efaaa157eb71bf14b2c0e790d5dc62014f5270dc
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-15 18:39:27 +0200

    Update with createPaymentIntent method

--- src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
@@ -1,17 +1,21 @@
 package com.evaldas.ecommerce.service;
 
 import com.evaldas.ecommerce.dao.CustomerRepository;
+import com.evaldas.ecommerce.dto.PaymentInfo;
 import com.evaldas.ecommerce.dto.Purchase;
 import com.evaldas.ecommerce.dto.PurchaseResponse;
 import com.evaldas.ecommerce.entity.Customer;
 import com.evaldas.ecommerce.entity.Order;
 import com.evaldas.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -19,8 +23,10 @@ public class CheckoutServiceImpl implements CheckoutService{
     private CustomerRepository customerRepository;
 
     @Autowired          // Optional, as we have single constructor
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key.secret}") String secretKey) {
         this.customerRepository = customerRepository;
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -51,6 +57,20 @@ public class CheckoutServiceImpl implements CheckoutService{
         customerRepository.save(customer);
 
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 
 
