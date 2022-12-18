759b6fd568210fa474e85135e73da7e6540c421c
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-18 17:07:39 +0200

    Add description in Stripe dashboard

--- src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/evaldas/ecommerce/service/CheckoutServiceImpl.java
@@ -69,6 +69,7 @@ public class CheckoutServiceImpl implements CheckoutService{
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "Nice Purchase");
 
         return PaymentIntent.create(params);
     }
